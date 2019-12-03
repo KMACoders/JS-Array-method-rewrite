1ca6a1349c20b1886d0f388960ba986d54087669
KMACoders <hungkids3003@gmail.com>
2019-12-03 14:14:42 +0700

    Update new rewrite method : Array join()

---
+++ joinRewrite.js
@@ -0,0 +1,24 @@
+//Declare an array
+let arr1 = ['Hung', 'Tuan', 'Hieu', 'Nam'];
+
+//This optional parameter indicates a string of characters to separate each element of the array
+//Default, if we don't pass any parameter to funtion, separator is comma.
+Array.prototype.joinRewrite = function(separator=','){
+  // Declare new string
+  let newStr='';
+  //Iterate through each element, and concat. 
+  for ( let i = 0; i < this.length; i++){
+    //No separator behind the last element !
+    if (i == this.length - 1 ){
+      newStr += this[i]; 
+    } else {
+      newStr += this[i] + separator;
+    }
+  }
+  //After concat all elements in array, we will return a newString.
+  return newStr;
+}
+
+//Test my rewrite join(). 
+let str = arr1.joinRewrite('');
+console.log(str);
